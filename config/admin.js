bf665b849483a10408a81ac0edd440c1a94b0dad
cristiancr20 <cristian.capa@unl.edu.ec>
2024-09-27 12:30:42 -0500

    Claves

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'qZrmX2mtYzNe/dqjBESz2Q=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'jnH4g+sYcn+gIfPcg4e4wA=='),
   },
   transfer: {
     token: {
