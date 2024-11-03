a2e37d9f9b7d1b289dea38c4cc6af0d91924d32c
Cristian Capa <68016998+cristiancr20@users.noreply.github.com>
2024-11-02 20:35:34 -0500

    Configuracion cors
    se configuro el cors del middlewares para  permitir solicitudes desde el frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,17 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://docutrack.vercel.app'], // Aquí pones tu dominio de Vercel
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      exposedHeaders: [],
+      credentials: true,
+      maxAge: 3600,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
