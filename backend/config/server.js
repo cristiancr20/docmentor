8caaa36e1d89ee5aa475005486f4bf072072642b
Cristian Capa <68016998+cristiancr20@users.noreply.github.com>
2025-02-01 22:27:42 -0500

    DOckerfiles, docker-compose corrections

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://computacion.unl.edu.ec/docmentorapi', // Ruta base donde se aloja Strapi
+  //url: 'https://computacion.unl.edu.ec/docmentorapi', // Ruta base donde se aloja Strapi
   app: {
     keys: env.array('APP_KEYS', ['crcode130900', 'Capitacr20']),
   },
