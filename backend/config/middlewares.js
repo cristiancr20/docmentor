112927972e96c383d159de6de73c4a71984eb12a
Cristian Capa <68016998+cristiancr20@users.noreply.github.com>
2025-05-21 08:55:02 -0500

    fix: middlewares

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,8 @@
-  module.exports = [
+/*   module.exports = [
     'strapi::logger',
     'strapi::errors',
     'strapi::security',
-    /*  'strapi::cors', */
+    /*  'strapi::cors', 
     {
       name: 'strapi::cors',
       config: {
@@ -12,7 +12,7 @@
       },
     },
 
-    /*   {
+       {
         name: 'strapi::cors',
         config: {
           origin: process.env.NODE_ENV === 'production'
@@ -24,7 +24,7 @@
           credentials: true,
           maxAge: 3600,
         },
-      }, */
+      }, 
     'strapi::poweredBy',
     'strapi::query',
     'strapi::body',
@@ -32,3 +32,39 @@
     'strapi::favicon',
     'strapi::public',
   ];
+ */
+
+  module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  /*  'strapi::cors', */
+  /*   {
+      name: 'strapi::cors',
+      config: {
+        origin: ['http://localhost:3000'], // Cambia esto por la URL de tu frontend
+        credentials: true,
+      },
+    }, */
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: process.env.NODE_ENV === 'production'
+        ? ['https://docmentor-unl.vercel.app']
+        : ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      exposedHeaders: [],
+      credentials: true,
+      maxAge: 3600,
+    },
+  },
+
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
